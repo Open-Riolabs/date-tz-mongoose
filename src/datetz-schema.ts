b76716499f866c6a2f009ba255e60886de8d78de
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2025-06-23 16:50:39 +0200

    Add Git hooks and initial project setup for DateTz package
    - Implemented various Git hooks including pre-rebase, pre-receive, prepare-commit-msg, push-to-checkout, sendemail-validate, update, and others to manage repository behavior and enforce policies.
    - Created a package.json and package-lock.json for the DateTz project, defining dependencies and scripts.
    - Added a readme.md file with comprehensive documentation for the DateTz class, detailing its constructor, properties, methods, and usage examples.
    - Developed a DateTzSchema for Mongoose to handle DateTz objects in MongoDB.
    - Configured TypeScript with tsconfig.json to support the project structure and compilation settings.

---
+++ src/datetz-schema.ts
@@ -0,0 +1,25 @@
+import { DateTz } from "@open-rlb/date-tz";
+import { IDateTz } from "@open-rlb/date-tz/idate-tz";
+import mongoose, { SchemaType } from "mongoose";
+
+
+export class DateTzSchema extends SchemaType {
+  constructor(key: string, options: any) {
+    super(key, options, 'DateTzSchema');
+  }
+
+  cast(data: any): IDateTz {
+    if (data instanceof DateTz) {
+      return data;
+    }
+    if (data && typeof data === 'object' && typeof data.timestamp === 'number' && typeof data.timezone === 'string') {
+      return new DateTz(data.timestamp, data.timezone);
+    }
+    // if (typeof data === 'string') {
+    //   return DateTz.parse(data, DateTz.defaultFormat);
+    // }
+    return undefined;
+  }
+}
+
+mongoose.Schema.Types['DateTzSchema'] = DateTzSchema;
